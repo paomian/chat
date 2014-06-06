932d6698101db3b09782d0ac194a256a5da70a33
paomian <tanglnan1994@gmail.com>
2014-06-06 22:42:24 +0800

    Initial Commit

---
+++ chat/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  chat
+//
+//  Created by ipaomian on 14-6-6.
+//  Copyright (c) 2014年 ipaomian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
